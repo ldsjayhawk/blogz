2e7ae60cae792339cf9dc6db086023c3257bf77c
Cris Altman <crisbecca@gmail.com>
2018-01-07 00:23:15 -0600

    error adding blog posts
    -receiving error adding post
    -need to finish single post page

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
-    __tablename__ = 'Blogging'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
@@ -19,22 +18,43 @@ class Blog(db.Model):
 
 @app.route('/', methods=['GET'])
 def index():
-
     if request.method == 'GET':
+        blogs = Blog.query.all()
         return render_template('blog_entries.html', title="Build A Blog", blogs=blogs)
 
 @app.route('/newpost', methods=['POST','GET'])
 def new_post():
     if request.method == 'GET':
-        return render_template('new_post.html', title="Build A Blog")
+        return render_template('new_post.html', title="Add New Post")
 
     if request.method =='POST':
         blog_title = request.form['blog-title']
         blog = request.form['blog']
-        #add error handling (is there a title? is there a blog post?)
+        blank_blog_title_error = ""
+        blank_blog_error = ""
+
+        if " " in blog_title:
+            blank_blog_title_error = "Blog title cannot be blank."
+
+        if " " in blog:
+            blank_blog_error = "Blog post cannot be blank."
+
+        new_blog = Blog(blog_title, body)
+        db.session.add(new_blog)
+        db.session.commit
         #create a blog object to put into database
         #add post to db and commit post to db
-        return render_template('blog_entries.html', title="Build A Blog", blogs=blogs)
+        blogs = Blog.query.all()
+        return render_template('blog_entries.html', title="Build A Blog", blogs=blogs, 
+            blank_blog_title_error=blank_blog_title_error, blank_blog_error=blank_blog_error)
+
+@app.route('/singlepost', methods=['POST','GET'])
+def single_post():
+    if request.method == 'GET':
+        blog_id = int(request.args.get['blog-id'])
+        blog = Task.query.get(task_id)        
+        return render_template('single_post.html', title="Build A Blog", blog=blog)
+
 
 if __name__ == '__main__':
     app.run()
