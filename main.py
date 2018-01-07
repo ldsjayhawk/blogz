119200993af588f6a5561607c534dd1fd0802a67
Cris Altman <crisbecca@gmail.com>
2018-01-06 18:49:00 -0600

    working db

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@local
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-blogs = []
+class Blog(db.Model):
+    __tablename__ = 'Blogging'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 @app.route('/', methods=['GET'])
 def index():
@@ -23,11 +31,10 @@ def new_post():
     if request.method =='POST':
         blog_title = request.form['blog-title']
         blog = request.form['blog']
-        blogs.append(blog)
         #add error handling (is there a title? is there a blog post?)
         #create a blog object to put into database
         #add post to db and commit post to db
         return render_template('blog_entries.html', title="Build A Blog", blogs=blogs)
 
-#if__name__ == 'main':
-app.run()
+if __name__ == '__main__':
+    app.run()
